766c83a4b5d1e4d766747e2f47087f17200494a7
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-02-19 17:09:36 +0100

    Better test case

--- test/fake-app/Resources/app.js
+++ test/fake-app/Resources/app.js
@@ -1,12 +1,61 @@
 
 var request = require('ti-superagent');
 
-request
-.get('http://10.1.23.23:8080')
-.set({
-	"X-Prova": "Prooooova"
-})
-.timeout(100)
-.end(function (res) {
-	console.log(JSON.stringify(res.headers, null, 2));
-});
+var config = require('config');
+
+var MAX_ERRORS = 3;
+var CHUNK = 500;
+var MUL = 2;
+var errors = 0;
+
+var DONE = {};
+
+function next(i) {
+  var req = request
+  .get(config.HOST)
+  .set({
+  	"X-Count": i
+  })
+//  .timeout(3000)
+  .end(function (err, res) {
+
+    //trace("Call for " + i);
+
+    if (DONE[i]) {
+      throw new Error("Duplicate callback!");
+    }
+    else if (err) {
+      DONE[i] = true;
+      errors += 1;
+      Ti.API.error("uaoo");
+      throw err;
+    }
+    else {
+      DONE[i] = true;
+      Ti.API.info("Concludes correctly for " + i + " and res is " + res.text.length);
+    	next(i * MUL);
+    }
+  });
+
+  req.xhr.timeout = 3e3;
+
+  /*timeoutId = setTimeout(function () {
+    errors += 1;
+    aborted = true;
+
+    if (errors <= MAX_ERRORS) {
+      Ti.API.info("I’ll try again for " + i);
+      next(i);
+    }
+    else {
+      Ti.API.error("Gross! Failed for " + i);
+    }
+  }, 1200);*/
+
+}
+
+next(CHUNK);
+
+function trace(msg) {
+  Ti.API.error((new Error(msg)).stack);
+}
