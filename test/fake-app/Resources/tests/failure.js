005b7feb5790aa8791f9d4be67a3c6b9c856ea7c
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-02-24 00:21:31 +0100

    Removed vestigial log.

--- test/fake-app/Resources/tests/failure.js
+++ test/fake-app/Resources/tests/failure.js
@@ -5,9 +5,6 @@ module.exports = failuretest;
 
 function failuretest(callback) {
   request.get("http://i-do-not-exist.com").end(function (err, res) {
-
-    Ti.API.error(JSON.stringify(err, null, 2));
-
     if (!err) callback(new Error("i-do-not-exist.com… exists…"));
     else callback(null);
   });
